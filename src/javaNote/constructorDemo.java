eeb13f8e08a49ba0f5ee7b3d69e1407cedf36f25
ChungCT <chungct0916@gmail.com>
2017-08-25 20:21:39 +0800

    construct2

--- src/javaNote/constructorDemo.java
+++ src/javaNote/constructorDemo.java
@@ -1,8 +1,37 @@
+package javaNote;
 
-public class constructorDemo {
+//constructor 
+class Car{
+	// initialize variable
+	private double gas =0;
+	private String color ="";
+	
+	// constructor no argument
+	Car(){
+		gas = 100;
+		color = "white";
+	}
+	
+	Car(int gas, String color){
+		this.gas = gas;
+		this.color = color;
+		
+	}
+	
+	void show(){
+		System.out.println("目前油量:"+gas+"\t"+"car color:"+color);
+	}
+	
+	
+}
+
+public class obj {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Car car1 = new Car(); //use no-argument constructor
+		car1.show();
+		Car car2 = new Car(80,"Red");//use constructor with arguments
+		car2.show();
 
 	}
 
--- src/javaNote/objDemo.java
+++ src/javaNote/objDemo.java
@@ -1,5 +1,6 @@
 package javaNote;
 
+//public and private variable
 class Car{
 	private double gas = 0;
 	public double speed = 0;
@@ -13,16 +14,21 @@ class Car{
 		return gas;
 	}
 	
-	void showSpeed(){
-		System.out.println("目前速度"+speed);
+	double showSpeed(){
+		return speed;
 	}
 }
-public class objDemo {
+public class obj {
 
 	public static void main(String[] args) {
-		Car car1 = new car();
-		car1.speed = 40;
-		System.out.println("目前速度:"+car.speed);
+		Car car1 = new Car();
+		car1.speed = 40; // speed variable is public
+		System.out.println("目前速度:"+car1.speed);
+		
+		//car1.gas = 100; // get error because gas is private
+		// build setter and getter method to set and get private gas variable
+		car1.set_gas(100);
+		System.out.println(car1.getgas());
 
 	}
 
